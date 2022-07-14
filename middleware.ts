70c39f914d942bff7f24dad93f6b5fc890c03ed5
pwalters <patrick.walters@angel.com>
2022-07-14 15:33:34 -0400

    rearranging the middleware

--- middleware.ts
+++ middleware.ts
@@ -16,22 +16,22 @@ const PUBLIC_FILE = /\.(.*)$/
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (
-      request.nextUrl.pathname.startsWith('/_next') ||
-      request.nextUrl.pathname.includes('/api/') ||
-      PUBLIC_FILE.test(request.nextUrl.pathname)
-  ) {
-    return
-  }
-
   const enableBetaLanguages = !!request?.cookies.get('ANGEL_BETA_LANGUAGE')
+  const validRequestedLocale = enableBetaLanguages || isPublicLocale(request.nextUrl.locale)
 
-  if (!enableBetaLanguages && !isPublicLocale(request.nextUrl.locale)) {
+  if ( request.nextUrl.pathname.startsWith('/_next') ||
+      request.nextUrl.pathname.includes('/api/') ||
+      PUBLIC_FILE.test(request.nextUrl.pathname) ||
+      validRequestedLocale ) {
+
+    console.log('exiting the middleware...')
+
+    return
+
+  } else {
     const rewrite = new URL(request.nextUrl.pathname, request.url)
     console.log("CAUSING A REWRITE!", { rewrite })
 
     return NextResponse.rewrite(rewrite)
   }
-
-  return
 }

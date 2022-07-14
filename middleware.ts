b829fb0ae86cf542e300a2f69fedc0e82d11211d
pwalters <patrick.walters@angel.com>
2022-07-14 15:28:03 -0400

    following the next docs

--- middleware.ts
+++ middleware.ts
@@ -12,8 +12,18 @@ function isPublicLocale(locale: string) {
   return isPublicLocale
 }
 
+const PUBLIC_FILE = /\.(.*)$/
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  if (
+      request.nextUrl.pathname.startsWith('/_next') ||
+      request.nextUrl.pathname.includes('/api/') ||
+      PUBLIC_FILE.test(request.nextUrl.pathname)
+  ) {
+    return
+  }
+
   const enableBetaLanguages = !!request?.cookies.get('ANGEL_BETA_LANGUAGE')
 
   if (!enableBetaLanguages && !isPublicLocale(request.nextUrl.locale)) {
@@ -23,13 +33,5 @@ export function middleware(request: NextRequest) {
     return NextResponse.rewrite(rewrite)
   }
 
-  return NextResponse.next()
-}
-
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: [
-    ...locales.beta.map((locale) => `/${locale}/:path*`),
-    ...locales.alpha.map((locale) => `/${locale}/:path*`),
-  ],
+  return
 }

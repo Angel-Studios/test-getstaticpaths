886c2ec27fb694138f056a336e914e73e1c96921
pwalters <patrick.walters@angel.com>
2022-07-14 14:26:29 -0400

    add the middleware-- is this the smoking gun!??!?!?!?

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import locales from '@/constants/locales'
+
+function isPublicLocale(locale: string) {
+  return !!locales.public.find((publicLocale) => publicLocale === locale)
+}
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const enableBetaLanguages = !!request?.cookies.get('ANGEL_BETA_LANGUAGE')
+
+  if (!enableBetaLanguages && !isPublicLocale(request.nextUrl.locale)) {
+    return NextResponse.rewrite(new URL(request.nextUrl.pathname, request.url))
+  }
+
+  return undefined
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    ...locales.beta.map((locale) => `/${locale}/:path*`),
+    ...locales.alpha.map((locale) => `/${locale}/:path*`),
+  ],
+}

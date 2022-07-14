c5d27ea198275287c55a6193cd2ef2fed5ac726d
pwalters <patrick.walters@angel.com>
2022-07-14 15:43:07 -0400

    adding logic back in?

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-// import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import locales from '@/constants/locales'
 
@@ -12,29 +12,27 @@ function isPublicLocale(locale: string) {
   return isPublicLocale
 }
 
-// const PUBLIC_FILE = /\.(.*)$/
+const PUBLIC_FILE = /\.(.*)$/
 
 export function middleware(request: NextRequest) {
-  // const enableBetaLanguages = !!request?.cookies.get('ANGEL_BETA_LANGUAGE')
-  // const validRequestedLocale = enableBetaLanguages || isPublicLocale(request.nextUrl.locale)
-  //
-  // if ( request.nextUrl.pathname.startsWith('/_next') ||
-  //     request.nextUrl.pathname.includes('/api/') ||
-  //     PUBLIC_FILE.test(request.nextUrl.pathname) ||
-  //     validRequestedLocale ) {
-  //
-  //   console.log('exiting the middleware...')
-  //
-  //   return
-  //
-  // } else {
-  //   const rewrite = new URL(request.nextUrl.pathname, request.url)
-  //   console.error("CAUSING A REWRITE!", { locale: request.nextUrl.locale, url: request.url, rewrite })
-  //
-  //   return NextResponse.rewrite(rewrite)
-  // }
+  const enableBetaLanguages = !!request?.cookies.get('ANGEL_BETA_LANGUAGE')
+  const validRequestedLocale = enableBetaLanguages || isPublicLocale(request.nextUrl.locale)
 
-  return
+  if ( request.nextUrl.pathname.startsWith('/_next') ||
+      request.nextUrl.pathname.includes('/api/') ||
+      PUBLIC_FILE.test(request.nextUrl.pathname) ||
+      validRequestedLocale ) {
+
+    console.log('exiting the middleware...')
+
+    return
+
+  } else {
+    const rewrite = new URL(request.nextUrl.pathname, request.url)
+    console.error("CAUSING A REWRITE!", { locale: request.nextUrl.locale, url: request.url, rewrite })
+
+    return NextResponse.rewrite(rewrite)
+  }
 }
 
 export const config = {
